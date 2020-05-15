a24a7446302e7f847cfd6f2d093298469df443d7
onomoegi <onomoegi@192.168.10.112>
2020-05-15 20:27:20 +0900

    hashimo

---
+++ kadai11/src/kadai11/Food.java
@@ -0,0 +1,13 @@
+package kadai11;
+
+public class Food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello World!");
+	System.out.println("私の好きな食べ物はパスタです。種類がたくさんあって自分で作るのも簡単"
+			+ "なのでよく食べます。");
+
+	}
+}
